c9af944bcfbe3dbf0984d8a966367bcecfe0e5cf
Luis Fernando <luisfernandinho094@gmail.com>
2026-05-05 21:48:43 -0300

    feat: setup spring project

---
+++ src/main/java/com/grilario/spring_commerce/SpringCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.grilario.spring_commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCommerceApplication.class, args);
+	}
+
+}
